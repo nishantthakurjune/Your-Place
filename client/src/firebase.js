e4890d3a9b0ec0d230f27de08750774b65acd6e0
nishantthakurjune <nishantthakurjune@gmail.com>
2024-12-16 16:02:08 +0530

    updated firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "yourplace-8acf8.firebaseapp.com",
+  projectId: "yourplace-8acf8",
+  storageBucket: "yourplace-8acf8.firebasestorage.app",
+  messagingSenderId: "495633333211",
+  appId: "1:495633333211:web:54e758a78f081b517445fe"
 };
 
 // Initialize Firebase
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Your Place is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
@@ -64,7 +64,7 @@ export default function Home() {
           to={'/search'}
           className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
         >
-          Let's get started...
+          Get started...
         </Link>
       </div>
 
